602250a91cdf23e315c68f70c59e39a2dcf09746
konstantinnovation <konstans@stuy.edu>
2025-12-12 13:08:07 -0500

    Add connection testing comments in basic_client.c
    Added comments for connection testing in main function.

--- basic_client.c
+++ basic_client.c
@@ -1,10 +1,15 @@
 #include "pipe_networking.h"
-
+//add your includes
 
 int main() {
-
+  /*DO NOT CHANGE THE CODE BELOW*/
   int to_server;
   int from_server;
-
   from_server = client_handshake( &to_server );
+  /*DO NOT CHANGE THE ABOVE CODE*/
+
+  
+  /*TEST YOUR CONNECTION:
+    now you can read/write from/to the server. Test to verify.
+  */
 }

3ff3cbe5385467bd2767d3d4cad7fa4ee9b59768
konstantinnovation <konstans@stuy.edu>
2025-12-12 13:09:09 -0500

    Add comments to basic_server.c
    Added comments for code clarity and structure.

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,12 @@
 #include "pipe_networking.h"
-
+//add your includes
 
 int main() {
-
+  //DO NOT CHANGE THE CODE BELOW  
   int to_client;
   int from_client;
-
   from_client = server_handshake( &to_client );
+  //DO NOT CHANGE THE CODE ABOVE
+
+  /*Now you can write code to test that your connection is valid by writing/reading to/from the client*/
 }
